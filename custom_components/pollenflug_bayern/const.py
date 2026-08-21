bbec39312802828e0bb2549b0a8748a333e29669
Gernot <119130972+GernotAlthammer@users.noreply.github.com>
2026-08-21 18:38:20 +0200

    Add constants for pollenflug_bayern component

---
+++ custom_components/pollenflug_bayern/const.py
@@ -0,0 +1,12 @@
+DOMAIN = "pollenflug_bayern"
+PLATFORMS = ["sensor"]
+
+API_URL = "https://epin.lgl.bayern.de/api/measurements?locations=DEVIEC"
+SENSOR_TYPES = [
+    "Abies", "Acer", "Aesculus", "Alnus", "Ambrosia", "Artemisia", "Asteraceae", 
+    "Betula", "Carpinus", "Castanea", "Chenopodium", "Corylus", "Cruciferae", 
+    "Cyperaceae", "Erica", "Fagus", "Fraxinus", "Fungus", "Galium", "Humulus", 
+    "Impatiens", "Juglans", "Larix", "Picea", "Pinaceae", "Pinus", "Plantago", 
+    "Platanus", "Poaceae", "Populus", "Quercus", "Quercus ilex", "Rumex", "Salix", 
+    "Sambucus", "Secale", "Taxus", "Tilia", "Ulmus", "Urtica", "Varia"
+]
