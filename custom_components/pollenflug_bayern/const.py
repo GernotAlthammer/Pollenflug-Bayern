71e937414aac3974c1c319dfff7776493d498576
Gernot <119130972+GernotAlthammer@users.noreply.github.com>
2026-08-21 20:26:05 +0200

    Refactor pollen mapping and sensor types

--- custom_components/pollenflug_bayern/const.py
+++ custom_components/pollenflug_bayern/const.py
@@ -1,50 +1,11 @@
 DOMAIN = "pollenflug_bayern"
 PLATFORMS = ["sensor"]
 
-# Mapping der wissenschaftlichen Namen der API zu den deutschen Namen
-POLLEN_MAPPING = {
-    "Abies": "Tanne",
-    "Acer": "Ahorn",
-    "Aesculus": "Rosskastanie",
-    "Alnus": "Erle",
-    "Ambrosia": "Ambrosia",
-    "Artemisia": "Beifuß",
-    "Asteraceae": "Korbblütler",
-    "Betula": "Birke",
-    "Carpinus": "Hainbuche",
-    "Castanea": "Kastanie",
-    "Chenopodium": "Gänsefuß",
-    "Corylus": "Hasel",
-    "Cruciferae": "Kreuzblütler",
-    "Cyperaceae": "Sauergrasgewächse",
-    "Erica": "Heidekraut",
-    "Fagus": "Buche",
-    "Fraxinus": "Esche",
-    "Fungus": "Pilzsporen",
-    "Galium": "Labkraut",
-    "Humulus": "Hopfen",
-    "Impatiens": "Springkraut",
-    "Juglans": "Walnuss",
-    "Larix": "Lärche",
-    "Picea": "Fichte",
-    "Pinaceae": "Kieferngewächse",
-    "Pinus": "Kiefer",
-    "Plantago": "Wegerich",
-    "Platanus": "Platane",
-    "Poaceae": "Gräser",
-    "Populus": "Pappel",
-    "Quercus": "Eiche",
-    "Quercus ilex": "Steineiche",
-    "Rumex": "Ampfer",
-    "Salix": "Weide",
-    "Sambucus": "Holunder",
-    "Secale": "Roggen",
-    "Taxus": "Eibe",
-    "Tilia": "Linde",
-    "Ulmus": "Ulme",
-    "Urtica": "Brennnessel",
-    "Varia": "Sonstige"
-}
-
-# Wir extrahieren die API-Schlüssel für unsere bestehende Logik
-SENSOR_TYPES = list(POLLEN_MAPPING.keys())
+SENSOR_TYPES = [
+    "Abies", "Acer", "Aesculus", "Alnus", "Ambrosia", "Artemisia", "Asteraceae", 
+    "Betula", "Carpinus", "Castanea", "Chenopodium", "Corylus", "Cruciferae", 
+    "Cyperaceae", "Erica", "Fagus", "Fraxinus", "Fungus", "Galium", "Humulus", 
+    "Impatiens", "Juglans", "Larix", "Picea", "Pinaceae", "Pinus", "Plantago", 
+    "Platanus", "Poaceae", "Populus", "Quercus", "Quercus ilex", "Rumex", "Salix", 
+    "Sambucus", "Secale", "Taxus", "Tilia", "Ulmus", "Urtica", "Varia"
+]
